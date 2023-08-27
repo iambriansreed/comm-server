03cf60456dc86e8695f60df4174a1966f182e374
Brian S. Reed <iambriansreed@gmail.com>
2023-08-27 08:03:47 -0400

    cleanup server

--- src/room-manager.ts
+++ src/room-manager.ts
@@ -8,58 +8,7 @@ export const newEvent = (event: Omit<RoomEvent, 'time' | 'uid'>): RoomEvent => (
 
 const rooms: Record<string, Room> = {
     main: {
-        events: [
-            newEvent({ data: { username: 'system' }, type: 'initialize', username: 'SYSTEM' }),
-            {
-                data: 'B Hey whats up?',
-                type: 'message',
-                username: 'brian',
-                time: Date.now(),
-                uid: '',
-            },
-            {
-                data: 'R Not much. What going on with you?',
-                type: 'message',
-                username: 'ryleigh',
-                time: Date.now() + 50000,
-                uid: '',
-            },
-            {
-                data: 'B Not much. What going on with you? Not much. What going on with you?',
-                type: 'message',
-                username: 'brian',
-                time: Date.now() + 100000,
-                uid: '',
-            },
-            {
-                data: 'R Not much. What going on with you? Not much.',
-                type: 'message',
-                username: 'ryleigh',
-                time: Date.now() + 150000,
-                uid: '',
-            },
-            {
-                data: 'R Not much. What going on with you? Not much. What going on with you? Not much. What going on with you?',
-                type: 'message',
-                username: 'ryleigh',
-                time: Date.now() + 150000,
-                uid: '',
-            },
-            {
-                data: 'R Not much. What going on with you? Not much. What going on with you? Not much. What going on with you?',
-                type: 'message',
-                username: 'ryleigh',
-                time: Date.now() + 150000,
-                uid: '',
-            },
-            {
-                data: 'b Not much. What going on with you? Not much. What going on with you? Not much. What going on with you? Not much. What going on with you? Not much. What going on with you?',
-                type: 'message',
-                username: 'brian',
-                time: Date.now() + 200000,
-                uid: '',
-            },
-        ],
+        events: [newEvent({ data: { username: 'system' }, type: 'initialize', username: 'SYSTEM' })],
         name: 'main',
         usernames: [],
     },
