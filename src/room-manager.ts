2c5738e95e8d0024f4cec91a73a4a1fc518cc421
Brian S. Reed <iambriansreed@gmail.com>
2023-08-27 14:40:24 -0400

    cleanup

--- src/room-manager.ts
+++ src/room-manager.ts
@@ -1,4 +1,62 @@
-import { Room, RoomEvent } from '@bsr-comm/types';
+import {
+    //
+    PayloadRoomJoin,
+    PayloadRoomLeave,
+    Room,
+    RoomEvent,
+} from '@bsr-comm/types';
+
+export const roomJoin = ({ username, room }: PayloadRoomJoin, callback: (room: Room | false) => void) => {
+    const joinEvent = newEvent({
+        type: 'join',
+        data: { username, room },
+        username: 'SYSTEM',
+    });
+
+    const { events, name, usernames } = rooms[room];
+
+    rooms[room] = {
+        name,
+        events: [...events, joinEvent],
+        usernames: [...usernames, username].filter((u, index, arr) => index === arr.findIndex((u1) => u1 === u)),
+    };
+
+    callback(rooms[room]);
+};
+
+export const roomLeave = ({ username, room }: PayloadRoomLeave, callback: (room: Room | false) => void) => {
+    const leaveEvent = newEvent({
+        type: 'leave',
+        data: { username, room },
+        username: 'SYSTEM',
+    });
+
+    const { events, name, usernames } = rooms[room];
+
+    rooms[room] = {
+        name,
+        events: [...events, leaveEvent],
+        usernames: usernames.filter((u) => username !== u),
+    };
+
+    callback(rooms[room]);
+};
+
+export const roomCreate = ({ username, room }: PayloadRoomJoin, callback: (room: Room | false) => void) => {
+    rooms[room] = {
+        name: room,
+        usernames: [username],
+        events: [
+            newEvent({
+                type: 'initialize',
+                data: { room, username },
+                username: 'SYSTEM',
+            }),
+        ],
+    };
+
+    callback(rooms[room]);
+};
 
 export const newEvent = (event: Omit<RoomEvent, 'time' | 'uid'>): RoomEvent => ({
     ...event,
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
-import rooms, { newEvent } from './room-manager';
+import rooms, { newEvent, roomCreate, roomJoin, roomLeave } from './room-manager';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import { SocketEvent, Room, PayloadLogin, PayloadEvent, RoomEvent, PayloadServerUpdate } from '@bsr-comm/types';
+import {
+    SocketEvent,
+    Room,
+    RoomEvent,
+    PayloadRoomJoin,
+    PayloadRoomLeave,
+    PayloadClientInit,
+    PayloadRoomEvent,
+} from '@bsr-comm/types';
 import data from './new-room-data';
 
 const HOST = process.env.HOST || process.env.RENDER_EXTERNAL_HOSTNAME!;
@@ -18,84 +26,37 @@ const io = new Server(httpServer, {
 
 io.on('connection', (socket) => {
     //
-    const onRoomJoin = (payload: PayloadLogin, callback: (room: Room) => void) => {
+    const onRoomJoin = (payload: PayloadRoomJoin, callback: (room: Room | false) => void) => {
         socket.join(payload.room);
 
         if (!rooms[payload.room]) {
-            rooms[payload.room] = {
-                name: payload.room,
-                usernames: [payload.username],
-                events: [
-                    newEvent({
-                        type: 'initialize',
-                        data: { ...payload },
-                        username: 'SYSTEM',
-                    }),
-                ],
-            };
-
-            socket.emit(SocketEvent.ServerUpdate, {
-                rooms: Object.keys(rooms),
-            } as PayloadServerUpdate);
+            roomCreate(payload, callback);
         } else {
-            const joinEvent = {
-                type: 'join',
-                data: { ...payload },
-                time: Date.now(),
-                username: 'SYSTEM',
-            } as RoomEvent;
-
-            const { events, name, usernames } = rooms[payload.room];
-
-            rooms[payload.room] = {
-                name,
-                events: [...events, joinEvent],
-                usernames: [...usernames, payload.username].filter(
-                    (u, index, arr) => index === arr.findIndex((u1) => u1 === u)
-                ),
-            };
-
-            socket.to(payload.room).emit(SocketEvent.EventFromServer, joinEvent);
+            roomJoin(payload, callback);
         }
 
         callback(rooms[payload.room]);
     };
 
-    const onRoomLeave = (payload: PayloadLogin, callback: (success: boolean) => void) => {
+    const onRoomLeave = (payload: PayloadRoomLeave, callback: (room: Room | false) => void) => {
         if (!rooms[payload.room]) {
             callback(false);
             return;
         }
 
-        const leaveEvent = newEvent({
-            type: 'leave',
-            data: { ...payload },
-            username: 'SYSTEM',
-        });
-
-        rooms[payload.room].usernames = rooms[payload.room].usernames.filter((u) => u !== payload.username);
-
-        rooms[payload.room].events.push(leaveEvent);
-
-        socket.to(payload.room).emit(SocketEvent.EventFromServer, leaveEvent);
-        socket.leave(payload.room);
-
-        callback(true);
+        roomLeave(payload, callback);
     };
 
-    const onEventFromClient = (
-        { data, room, type, username }: PayloadEvent<any>,
-        callback: (newEvent: RoomEvent) => void
-    ) => {
+    const onRoomEvent = ({ data, room, type, username }: PayloadRoomEvent, callback: (newEvent: RoomEvent) => void) => {
         const event = newEvent({ data, type, username });
 
         rooms[room].events.push(event);
-        socket.to(room).emit(SocketEvent.EventFromServer, event);
+        socket.to(room).emit(SocketEvent.RoomEvent, event);
 
         callback(event);
     };
 
-    const onNewRoom = (callback: (roomName: string) => void) => {
+    const onRoomNewName = (callback: (roomName: string) => void) => {
         function random(min: number, max: number) {
             return Math.floor(Math.random() * (max - min + 1) + min);
         }
@@ -109,15 +70,15 @@ io.on('connection', (socket) => {
         );
     };
 
-    const onClientInit = (callback: (serverUpdate: PayloadServerUpdate) => {}) => {
+    const onClientInit = (callback: (serverUpdate: PayloadClientInit) => {}) => {
         callback({ rooms: Object.keys(rooms) });
     };
 
     socket.on(SocketEvent.ClientInit, onClientInit);
-    socket.on(SocketEvent.NewRoom, onNewRoom);
+    socket.on(SocketEvent.RoomEvent, onRoomEvent);
     socket.on(SocketEvent.RoomJoin, onRoomJoin);
     socket.on(SocketEvent.RoomLeave, onRoomLeave);
-    socket.on(SocketEvent.EventFromClient, onEventFromClient);
+    socket.on(SocketEvent.RoomNewName, onRoomNewName);
 });
 
 httpServer.listen(PORT, HOST, () => {
