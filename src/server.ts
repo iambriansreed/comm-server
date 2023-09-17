0be85918bbfc0ae52b9771348101da1a56e93521
Brian S. Reed <iambriansreed@gmail.com>
2023-09-17 19:48:42 -0400

    extract reusable func

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,7 @@
-import channelManager from './channels-manager';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import {
-    ChannelAction,
-    ChannelEvent,
-    ChannelNameResponse,
-    ClientStatusResponse,
-    ErrorResponse,
-    EventAction,
-    LoginResponse,
-    LogoutResponse,
-    SocketEvent,
-} from '@bsr-comm/types';
+import { SocketServer } from '@bsr-comms/utils';
+import crypto from 'crypto';
 
 const HOST = process.env.HOST || process.env.RENDER_EXTERNAL_HOSTNAME!;
 const PORT = Number(process.env.PORT);
@@ -25,59 +15,10 @@ const io = new Server(httpServer, {
     },
 });
 
+const getUid = () => crypto.randomBytes(16).toString('hex');
+
 io.on('connection', (socket) => {
-    socket.on(
-        SocketEvent.ClientStatus,
-        //
-        (callback?: (serverUpdate: ClientStatusResponse) => void) => {
-            const channels = channelManager.getChannels();
-            callback?.({ channels } as ClientStatusResponse);
-        }
-    );
-
-    socket.on(
-        SocketEvent.ChannelEvent,
-        //
-        (payload: EventAction, callback?: (event: ChannelEvent | null) => void) => {
-            if (typeof payload.data !== 'object') {
-                callback?.(null);
-                return;
-            }
-
-            const event = channelManager.addEvent(payload);
-
-            socket.broadcast.to(payload.channel).emit(SocketEvent.ChannelEvent, event);
-            socket.broadcast.to(payload.channel).emit(SocketEvent.ChannelEvent, event);
-            callback?.(event);
-        }
-    );
-
-    socket.on(SocketEvent.ChannelName, (callback?: (response: ChannelNameResponse) => void) =>
-        callback?.({ name: channelManager.getNewName() } as ChannelNameResponse)
-    );
-
-    socket.on(
-        SocketEvent.ChannelLogin,
-        (payload: ChannelAction, callback?: (response: LoginResponse | ErrorResponse) => void) => {
-            const response = channelManager.channelLogin(payload);
-
-            if ('error' in response) {
-                callback?.(response);
-                return;
-            }
-
-            socket.join(payload.channel);
-            socket.broadcast.emit(SocketEvent.ChannelLogin, response);
-            callback?.(response);
-        }
-    );
-
-    socket.on(SocketEvent.ChannelLogout, (payload: ChannelAction, callback?: (response: LogoutResponse) => void) => {
-        const response = channelManager.channelLogout(payload);
-        socket.broadcast.emit(SocketEvent.ChannelLogout, response);
-        socket.leave(payload.channel);
-        callback?.(response);
-    });
+    SocketServer({ socket, getUid, maxUsers: 5 });
 });
 
 httpServer.listen(PORT, HOST, () => {
